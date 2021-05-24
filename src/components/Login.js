7a5c929c0a96d8d78bf24fae5df13a23567c7aca
AmaanKhan2000 <amaankhxn2000@gmail.com>
2021-05-24 22:34:10 +0300

    Login Push #1

---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const Login = (props) =>{
+  return (
+    <Container>
+      <Content>
+        <h1>Content</h1>
+      </Content>
+    </Container>
+  )
+};
+
+
+const Container = styled.section`
+overflow: hidden;
+display: flex;
+flex-direction: column;
+text-align: center;
+height: 100vh;
+`;
+
+const Content = styled.div `
+margin-bottom: 10vw;
+width: 100%;
+position: relative;
+min-height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 80px 40px;
+height: 100%;
+`;
+
+export default Login;
